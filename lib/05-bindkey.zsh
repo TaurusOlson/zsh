040eb70f997705734ab70854331c1c00f50ed293
TaurusOlson <taurusolson@gmail.com>
2012-01-03 15:23:23 +0100

    New bindkeys

--- lib/05-bindkey.zsh
+++ lib/05-bindkey.zsh
@@ -8,3 +8,6 @@ bindkey "^N" complete-word
 bindkey "^X" delete-char
 bindkey "^R" history-search-forward
 bindkey "^T" history-search-backward
+bindkey '^X^A' f-complete    # C-x C-a to do f-complete (fils and directories)
+bindkey '^X^F' f-complete-f  # C-x C-f to do f-cmplete-f (only files)
+bindkey '^X^D' f-complete-d  # C-x C-d to do f-complete-d (only directories))))
